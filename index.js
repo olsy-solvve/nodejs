5aaaccaf0387fcf31582185685a8beb73bca0151
olsy <olsy@solvve.com>
2021-11-04 17:12:48 +0200

    nodejs courses ~2

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-const chalk = require('chalk'); // это модуль
-const text = require('./data.js'); // это соседний файл
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-console.log( chalk.red( text ) );
+const server = http.createServer((req, res) => {
+    const  file = req.url === '/' ? 'index.html' :  req.url;
+
+    // let file = req.url;
+    // if (req.url === '/') {
+    //     file = 'index.html';
+    // }
+
+    const filePath = path.join(__dirname, 'public', file);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            res.end('<h2 style="color: red">page or file not found</h2>');
+        }
+
+        let contentType = 'text/html';
+        const fileExt = path.extname(filePath);
+
+        // .html, .css, .js
+        if (fileExt === '.css') {
+            contentType = 'text/css';
+        } else if (fileExt === '.js') {
+            contentType = 'text/javascript';
+        }
+
+        res.writeHead(200, {
+            'Content-Type': contentType
+        });
+
+        res.end(content);
+    });
+
+    // if (req.url === '/' || req.url === '/index.html') {
+    //     fs.readFile(filePath, (err, content) => {
+    //         if (err) {
+    //             throw err;
+    //         }
+
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         });
+
+    //         res.end(content);
+    //     });
+    // } else {
+        // res.writeHead(404, {
+        //     'Content-Type': 'text/html'
+        // });
+        // res.end('<h2 style="color: red">page not found</h2>');
+    // }
+});
+
+server.listen(3000, () => {
+    console.log('Server has been started!');
+});
